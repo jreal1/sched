4a7626ddac8e92d9bd45132bc9e7f1f099e46b86
Jonathan Real <jon.c.real@gmail.com>
2023-03-19 15:41:00 -1000

    update

--- job-type/sched-cron-start.py
+++ job-type/sched-cron-start.py
@@ -1,6 +1,9 @@
 import psycopg2, sys, json, socket, datetime
 from confluent_kafka import Producer, Consumer, KafkaError, KafkaException
 
+#job_id should be the first and only argument
+job_id = sys.argv.pop()
+
 conn = psycopg2.connect(database="scheduler", user = "postgres", password = "postgrespw", host = "host.docker.internal", port = "32768")
 cur = conn.cursor()
 
@@ -12,14 +15,14 @@ producer_conf = {
 producer = Producer(producer_conf)
 
 # Update job_status table that job is starting
-sql = 'insert into job_status (timestamp, job_id, status, message) VALUES (\'%s\',%s,\'Start\',\'\');' % (datetime.datetime.now(), vals["job_id"])
+sql = 'insert into job_status (timestamp, job_id, status, message) VALUES (\'%s\',%s,\'Start\',\'\');' % (datetime.datetime.now(), sys.argv[1])
 cur.execute(sql)
 print(sql)
 conn.commit()
 
 # Do work
-print(vals["job_id"])
+print('Signaling completion of job_id=' + sys.argv[1])
 
 # Signal complete
-producer.produce('sched-job-complete', value='{"timestamp":"%s", "job_id":"%s", "status":"Success", "message":""}' % (datetime.datetime.now(), vals["job_id"]))
+producer.produce('sched-job-complete', value='{"timestamp":"%s", "job_id":"%s", "status":"Success", "message":""}' % (datetime.datetime.now(), sys.argv[1]))
 producer.flush()
--- test/jobdata.pgsql
+++ test/jobdata.pgsql
@@ -1,6 +1,6 @@
 truncate table job_dag, job_dag_tc, job_type, job cascade; --also truncates job table
 insert into job_type (job_type_id, job_type_name) OVERRIDING SYSTEM VALUE values (1, 'pyjob');
-insert into job (job_id, job_type_id, job_name) OVERRIDING SYSTEM VALUE values (1, 1, 'A');
+insert into job (job_id, job_type_id, job_name) OVERRIDING SYSTEM VALUE values (1, 2, 'A');
 insert into job (job_id, job_type_id, job_name) OVERRIDING SYSTEM VALUE values (2, 1, 'B');
 insert into job (job_id, job_type_id, job_name) OVERRIDING SYSTEM VALUE values (3, 1, 'C');
 insert into job_dag (job_id, parent_job_id) values (2,1),(3,2);
